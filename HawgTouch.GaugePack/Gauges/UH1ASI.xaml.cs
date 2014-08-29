9cbc65fc5288bd8062a1101fbc8f8e7ac044103c
cleardarkz <cleardarkz@gmail.com>
2014-08-29 10:16:41 +0300

    refactor

--- HawgTouch.GaugePack/Gauges/UH1ASI.xaml.cs
+++ HawgTouch.GaugePack/Gauges/UH1ASI.xaml.cs
@@ -55,7 +55,6 @@ namespace HawgTouch.GaugePack.Gauges
             this.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
                        (Action)(() =>
                        {
-                           RotateTransform rt = new RotateTransform();
                            // (Start Angle - End Angle) / (Start Value - End Value) * (Actual Value - Start Value ) + Start Angle
                            // 150 = 346.2 = 1.0
                            // 120 = 281.7 = 0.825
@@ -66,75 +65,68 @@ namespace HawgTouch.GaugePack.Gauges
                            // 30 = 66.5 = 0.19
                            // 20 = 23 = 0.075
                            // 0 = 0
+
+                           RotateTransform rt = new RotateTransform();
+                           double startAngle = 0 ,endAngle = 0,startValue = 0,endValue = 0;
+                           rt.Angle = 0.0;
                            if (value > 0.0 && value <= 0.075)
                            {
-                               double startAngle = 0.0;
-                               double endAngle = 23.1;
-                               double startValue = 0.0;
-                               double endValue = 0.075;
-                               rt.Angle = (startAngle - endAngle) / (startValue - endValue) * (value - startValue) + startAngle;
+                               startAngle = 0.0;
+                               endAngle = 23.1;
+                               startValue = 0.0;
+                               endValue = 0.075;
                            }
                            else if (value > 0.075 && value <= 0.19)
                            {
-                               double startAngle = 23.1;
-                               double endAngle = 66.5;
-                               double startValue = 0.075;
-                               double endValue = 0.19;
-                               rt.Angle = (startAngle - endAngle) / (startValue - endValue) * (value - startValue) + startAngle;
+                               startAngle = 23.1;
+                               endAngle = 66.5;
+                               startValue = 0.075;
+                               endValue = 0.19;
                            }
                            else if (value > 0.19 && value <= 0.32)
                            {
-                               double startAngle = 66.5;
-                               double endAngle = 109.0;
-                               double startValue = 0.19;
-                               double endValue = 0.32;
-                               rt.Angle = (startAngle - endAngle) / (startValue - endValue) * (value - startValue) + startAngle;
+                               startAngle = 66.5;
+                               endAngle = 109.0;
+                               startValue = 0.19;
+                               endValue = 0.32;
                            }
                            else if (value > 0.32 && value <= 0.395)
                            {
-                               double startAngle = 109.0;
-                               double endAngle = 135.5;
-                               double startValue = 0.32;
-                               double endValue = 0.395;
-                               rt.Angle = (startAngle - endAngle) / (startValue - endValue) * (value - startValue) + startAngle;
+                               startAngle = 109.0;
+                               endAngle = 135.5;
+                               startValue = 0.32;
+                               endValue = 0.395;
                            }
                            else if (value > 0.395 && value <= 0.44)
                            {
-                               double startAngle = 135.5;
-                               double endAngle = 152.3;
-                               double startValue = 0.395;
-                               double endValue = 0.44;
-                               rt.Angle = (startAngle - endAngle) / (startValue - endValue) * (value - startValue) + startAngle;
+                               startAngle = 135.5;
+                               endAngle = 152.3;
+                               startValue = 0.395;
+                               endValue = 0.44;
                            }
                            else if (value > 0.44 && value <= 0.55)
                            {
-                               double startAngle = 152.3;
-                               double endAngle = 195.4;
-                               double startValue = 0.44;
-                               double endValue = 0.55;
-                               rt.Angle = (startAngle - endAngle) / (startValue - endValue) * (value - startValue) + startAngle;
+                               startAngle = 152.3;
+                               endAngle = 195.4;
+                               startValue = 0.44;
+                               endValue = 0.55;
                            }
                            else if (value > 0.55 && value <= 0.825)
                            {
-                               double startAngle = 195.4;
-                               double endAngle = 281.7;
-                               double startValue = 0.55;
-                               double endValue = 0.825;
-                               rt.Angle = (startAngle - endAngle) / (startValue - endValue) * (value - startValue) + startAngle;
+                               startAngle = 195.4;
+                               endAngle = 281.7;
+                               startValue = 0.55;
+                               endValue = 0.825;
                            }
                            else if (value > 0.825 && value <= 1.0)
                            {
-                               double startAngle = 281.7;
-                               double endAngle = 346.2;
-                               double startValue = 0.825;
-                               double endValue = 1.0;
-                               rt.Angle = (startAngle - endAngle) / (startValue - endValue) * (value - startValue) + startAngle;
-                           }
-                           else
-                           {
-                               rt.Angle = 0.0;
+                               startAngle = 281.7;
+                               endAngle = 346.2;
+                               startValue = 0.825;
+                               endValue = 1.0;
                            }
 
+                           rt.Angle = (startAngle - endAngle) / (startValue - endValue) * (value - startValue) + startAngle;
                            this.SpeedNeedle.RenderTransform = rt;
                            Digital.Text = value.ToString();
                        }));
